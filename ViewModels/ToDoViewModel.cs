ac9de1a1c26ce2578348e0c2a66a7571e651c393
Manuela Ramos Fonseca <135083731+MmanuelaFfonseca251106@users.noreply.github.com>
2023-11-06 19:58:01 -0300

    Initial commit

---
+++ ViewModels/ToDoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MVVM_API_SampleProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_API_SampleProject.ViewModels
+{
+    public class ToDoViewModel
+    {
+        public ToDo ToDo { get; set; }
+
+        public ToDoViewModel()
+        {
+            ToDo = new ToDo()
+            {
+                UserId = 1,
+                Id = 2,
+                Title = "delectus aut autem",
+                Completed = true
+            };
+        }
+    }
+}
---
+++ Views/PostView.xaml.cs
@@ -0,0 +1,12 @@
+using MVVM_API_SampleProject.ViewModels;
+
+namespace MVVM_API_SampleProject.Views;
+
+public partial class PostView : ContentPage
+{
+	public PostView()
+	{
+		InitializeComponent();
+		BindingContext = new PostViewModel();
+	}
+}
